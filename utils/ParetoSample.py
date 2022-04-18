f19926e621b16e1a7d9a69d14c35fe33f447b866
MiguelTaibo <m.taibo@alumnos.upm.es>
2022-04-18 17:23:01 +0000

    solve averaged haussdorf distance

--- utils/ParetoSample.py
+++ utils/ParetoSample.py
@@ -12,8 +12,7 @@ def generateLinspaceGrid(N: int, D: int):
         grid = [ tf.tile(g,[N]) if idx<=o else tf.repeat(g,N) for idx,g in enumerate(grid)]
 
     grid = tf.convert_to_tensor(grid)
-    print("grid shape: ", grid.shape)
-
+    
     comp = np.unique(np.array([grid[:,a] for a in range(grid.shape[1])]),axis=0).shape
     if not (comp[0]==N**D and comp[1]==D):
         raise Exception("Grid has not correct dimensions or return values")
